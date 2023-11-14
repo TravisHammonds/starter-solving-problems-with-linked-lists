8d2b508d951770c8f7415510660e4fa062ce19e2
Travis Hammonds <travishammonds2@gmail.com>
2023-11-14 12:49:58 -0500

    completed algos

--- src/josephus/josephus.js
+++ src/josephus/josephus.js
@@ -1,3 +1,28 @@
-function josephus(list, m) {}
+function josephus(list, m) {
+  //   declare variable node and initialize it to the head
+  let node = list.head;
+  //   declare variable tail and initialize it to the last node in the list
+  const tail = list.fint((node) => !node.next);
+  //   assign the next pointer of the tail to the head of the list
+  tail.next = list.head;
+  //   while there are more than one nodes left do:
+  while (node.next != node) {
+    //     move node to node.next m times
+    for (let i = 1; i < m; i++) {
+      node = node.next;
+    }
+    //     if the next pointer of the current node points to the head
+    if(node.next === list.head){
+    //       set the head to the next node after the head
+    list.head = list.head.next;
+    }
+    //     set node.next  to  node.next.next
+    node.next = node.next.next;
+    //     node = node.next;
+    node = node.next
+  }
+  //   return node.value;
+  return node.value;
+}
 
 module.exports = josephus;
--- src/swap/swap.js
+++ src/swap/swap.js
@@ -4,6 +4,41 @@
  * @param {Node} x
  * @param {Node} y
  */
-function swap(list, x, y) {}
+function swap(list, x, y) {
+  //   if the list is empty then
+  if (!list.head) {
+    //     return  the list
+    return list;
+  }
+  //   declare a variable x_next and initialize it to the next pointer of x
+  const x_next = x.nextSibling;
+  //   declare a variable x_prev and initialize it to the previous node to x, null if x is the head
+  const x_prev = list.findWithPrevious((node) => node === x)[1];
+  //   declare a variable y_prev and initialize it to the previous node to y, null if y is the head
+  const y_prev = list.findWithPrevious((node) => node === y)[1];
+
+  //   set the next pointer of x to the next pointer of y
+  x.next = y.next;
+  //   set the next pointer of y to x_next
+  y.next = x_next;
+  //   if x is not the head  of the list then
+  if (x_prev) {
+    //     set next pointer of x_prev to y
+    x_prev.next = y;
+  } else {
+    //     set head to y
+    list.head = y;
+  }
+  //   if y is not the head  of the list then
+  if (y_prev) {
+    //     set next pointer of y_prev to x
+    y_prev.next = x;
+  } else {
+    //     set head to x
+    list.head = x;
+  }
+  //   return the list
+  return list;
+}
 
 module.exports = swap;
